2dc0f61c8fe90a47042567cf7eedb005df8246a1
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-02-20 15:24:32 +0100

    Push Inicial Con JavaFX
    Push Inicial con JavaFX y pantallita

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module ShadowAuction {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens shadowAuction;
+
+}
---
+++ src/shadowAuction/mainController.java
@@ -0,0 +1,4 @@
+package shadowAuction;
+
+public class mainController {
+}
