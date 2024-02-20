fc5acaffd95580e9693805cd1c0e33875f2f23e6
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-02-20 18:24:33 +0100

    Push con FadeIn

--- src/main/java/edu/shadowauction/shadowauction/Main.java
+++ src/main/java/edu/shadowauction/shadowauction/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 640);
         stage.setTitle("Shadow Auction");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/shadowauction/shadowauction/MainController.java
+++ src/main/java/edu/shadowauction/shadowauction/MainController.java
@@ -1,14 +1,27 @@
 package edu.shadowauction.shadowauction;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ImageView logoShadowAuction;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        FadeTransition ft = new FadeTransition(Duration.seconds(2), logoShadowAuction);
+        ft.setFromValue(0.0);
+        ft.setToValue(1.0);
+        ft.play();
     }
 }
