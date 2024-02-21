27bb2f17cdc38061ce3ff9d7bcdd69e7277c8784
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-02-21 01:40:03 +0100

    Testeos de Disclaimer

--- src/main/java/edu/shadowauction/shadowauction/MainController.java
+++ src/main/java/edu/shadowauction/shadowauction/MainController.java
@@ -3,16 +3,29 @@ package edu.shadowauction.shadowauction;
 import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
     @FXML
     private ImageView logoShadowAuction;
 
@@ -31,5 +44,14 @@ public class MainController {
         ft.setFromValue(0.0);
         ft.setToValue(1.0);
         ft.play();
+    }
+
+    @FXML
+    protected void switchScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Disclaimer.view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
