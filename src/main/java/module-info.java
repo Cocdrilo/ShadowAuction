05aaa33ca7fe317df121e784c1c83c378ef9b52d
Dbp2401 <dbp24012003@gmail.com>
2024-02-26 12:30:22 +0100

    Reading Database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module edu.shadowauction.shadowauction {
     requires javafx.media;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens edu.shadowauction.shadowauction to javafx.fxml;
     exports edu.shadowauction.shadowauction;
