cdbfe3b78ddf01e2c23e0d7c607ae4d68dbd1947
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-04-02 17:21:58 +0200

    auctioneer + start to build buttons

--- src/main/java/edu/shadowauction/shadowauction/AuctionController.java
+++ src/main/java/edu/shadowauction/shadowauction/AuctionController.java
@@ -7,9 +7,15 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+
+import java.io.FileNotFoundException;
+
 public class AuctionController{
+
+    private Auctioneer genericAuctioneer;
 
     @FXML
     private Button temporizador;
@@ -17,14 +23,32 @@ public class AuctionController{
     private Label labelTimer;
     @FXML
     private AnchorPane parentNode;
+    @FXML
+    private Label auctioneerLabel;
+    @FXML
+    private ImageView auctioneerImage;
     private Timeline timeline;
     private int seconds = 5 ;
     private int milliseconds = 0;
     private Thread hilo;
 
+    public AuctionController() throws FileNotFoundException {
+        this.genericAuctioneer = new Auctioneer("src/main/resources/images/christies-auction.jpg");
+    }
+
+    public void initialize() {
+        showAuctioneerAfterFade();
+    }
+
+    private void showAuctioneerAfterFade() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.5), event ->         auctioneerImage.setImage(genericAuctioneer.getAuctioneerImage())));
+        timeline.play();
+    }
+
 
     @FXML
     private void onActionBotonTimer(){
+        auctioneerLabel.setText(genericAuctioneer.obtenerFraseAleatoria());
         start();
     }
 
---
+++ src/main/java/edu/shadowauction/shadowauction/Auctioneer.java
@@ -0,0 +1,42 @@
+package edu.shadowauction.shadowauction;
+
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Auctioneer {
+
+    private Image auctioneerImage;
+    private ArrayList<String> frases;
+
+    public Auctioneer(String imageURL) throws FileNotFoundException {
+        frases = new ArrayList<String>();
+        this.auctioneerImage = new Image(new FileInputStream(imageURL));
+        cargarFrases();
+    }
+
+    public Image getAuctioneerImage() {
+        return auctioneerImage;
+    }
+
+    private void cargarFrases() {
+        frases.add("¿Quién da más?");
+        frases.add("¡Vamos, valiente! ¿Alguien supera esta oferta?");
+        frases.add("¡El momento de la oportunidad es ahora!");
+        frases.add("¿Algún interesado en llevarse este objeto a casa?");
+        frases.add("¡Suban esas pujas, caballeros!");
+        frases.add("¡La competencia está reñida, pero aún hay espacio para una oferta más!");
+        frases.add("¡Este es su momento para brillar!");
+        frases.add("¿Quién se lleva este tesoro a casa hoy?");
+        frases.add("¡No dejen que se les escape esta oportunidad única!");
+        frases.add("¡Un objeto tan valioso merece una puja aún mayor!");
+        frases.add("¡Hagamos que el valor de este objeto suba aún más alto!");
+    }
+
+    public String obtenerFraseAleatoria() {
+        int indiceAleatorio = (int) (Math.random() * frases.size());
+        return frases.get(indiceAleatorio);
+    }
+}
