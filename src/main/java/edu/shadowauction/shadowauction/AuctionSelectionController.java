cfbd507f5e6c29232cc966b1235502a615082a12
Dbp2401 <dbp24012003@gmail.com>
2024-04-11 16:18:54 +0200

    Selección de Subastas P2

--- src/main/java/edu/shadowauction/shadowauction/AuctionSelectionController.java
+++ src/main/java/edu/shadowauction/shadowauction/AuctionSelectionController.java
@@ -2,12 +2,12 @@ package edu.shadowauction.shadowauction;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class AuctionSelectionController {
@@ -19,41 +19,47 @@ public class AuctionSelectionController {
 
     private ArrayList<String> test;
 
-
     @FXML
     private void onActionBotonTimer() {
-        testScroll();
+        test();
     }
-
+    private ArrayList<Auction> subastas;
     @FXML
-    public void testScroll() {
-        // Crear una lista de cadenas de ejemplo
-        ArrayList<String> stringList = new ArrayList<>();
-        stringList.add("String 1");
-        stringList.add("String 2");
-        stringList.add("String 3");
-        stringList.add("String 4");
-        stringList.add("String 5");
-        stringList.add("String 6");
-        stringList.add("String 7");
-        stringList.add("String 8");
-        stringList.add("String 9");
-        stringList.add("String 10");
+    private void test() {
+        // Inicializar la lista de subastas
+        subastas = new ArrayList<>();
+        subastas.add(new Auction("Subasta 1", LocalDateTime.of(2024, 4, 11, 10, 0), LocalDateTime.of(2024, 4, 11, 12, 0)));
+        subastas.add(new Auction("Subasta 2", LocalDateTime.of(2024, 4, 12, 15, 0), LocalDateTime.of(2024, 4, 12, 17, 0)));
+        subastas.add(new Auction("Subasta 3", LocalDateTime.of(2024, 4, 13, 9, 0), LocalDateTime.of(2024, 4, 13, 11, 0)));
+        subastas.add(new Auction("Subasta 4", LocalDateTime.of(2024, 4, 11, 10, 0), LocalDateTime.of(2024, 4, 11, 12, 0)));
+        subastas.add(new Auction("Subasta 5", LocalDateTime.of(2024, 4, 12, 15, 0), LocalDateTime.of(2024, 4, 12, 17, 0)));
+        subastas.add(new Auction("Subasta 6", LocalDateTime.of(2024, 4, 13, 9, 0), LocalDateTime.of(2024, 4, 13, 11, 0)));
+        subastas.add(new Auction("Subasta 7", LocalDateTime.of(2024, 4, 11, 10, 0), LocalDateTime.of(2024, 4, 11, 12, 0)));
+        subastas.add(new Auction("Subasta 8", LocalDateTime.of(2024, 4, 12, 15, 0), LocalDateTime.of(2024, 4, 12, 17, 0)));
+        subastas.add(new Auction("Subasta 9", LocalDateTime.of(2024, 4, 13, 9, 0), LocalDateTime.of(2024, 4, 13, 11, 0)));
+        subastas.add(new Auction("Subasta 10", LocalDateTime.of(2024, 4, 11, 10, 0), LocalDateTime.of(2024, 4, 11, 12, 0)));
+        subastas.add(new Auction("Subasta 11", LocalDateTime.of(2024, 4, 12, 15, 0), LocalDateTime.of(2024, 4, 12, 17, 0)));
+        subastas.add(new Auction("Subasta 12", LocalDateTime.of(2024, 4, 13, 9, 0), LocalDateTime.of(2024, 4, 13, 11, 0)));
 
-        // Crear un TextArea para mostrar las cadenas
-        TextArea textArea = new TextArea();
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
+        // Crear un VBox para contener los botones de subasta
+        VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(10));
 
-        // Agregar las cadenas al TextArea
-        for (String str : stringList) {
-            textArea.appendText(str + "\n");
+        // Agregar un botón para cada subasta
+        for (Auction subasta : subastas) {
+            String buttonText = subasta.getNombre() + " - Inicio: " + subasta.getInicio() + " - Fin: " + subasta.getFin();
+            Button button = new Button(buttonText);
+            button.setOnAction(event -> {
+                // Realizar acciones según la subasta seleccionada
+                System.out.println("Subasta seleccionada: " + subasta.getNombre());
+                System.out.println("Inicio: " + subasta.getInicio());
+                System.out.println("Fin: " + subasta.getFin());
+            });
+            vbox.getChildren().add(button);
         }
 
-        // Crear un ScrollPane y agregar el TextArea
-        scrollPane.setContent(textArea);
-
+        // Crear un ScrollPane y agregar el VBox
+        scrollPane.setContent(vbox);
     }
-
-
 }
