f2f3280bf6d93e06f339206c4a2643a8887e28fb
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-05-09 13:32:49 +0200

    RecepitsScreen

--- src/main/java/edu/shadowauction/shadowauction/ReceiptsController.java
+++ src/main/java/edu/shadowauction/shadowauction/ReceiptsController.java
@@ -4,8 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReceiptsController {
 
@@ -17,6 +19,8 @@ public class ReceiptsController {
     public AnchorPane parentNode;
     @FXML
     public Label username;
+    @FXML
+    private AnchorPane receiptsPane;
 
     private FadeUtilityClass fader;
 
@@ -26,6 +30,7 @@ public class ReceiptsController {
 
     public void initialize(){
         username.setText(Usuario.getInstance(null,null).getUsername());
+        loadReceipts();
     }
 
     @FXML
@@ -35,6 +40,35 @@ public class ReceiptsController {
     @FXML
     public void myProfileOnAction() throws IOException {
         fader.fadeNextScene(parentNode,2,"MyProfile.view.fxml");
+    }
+
+    private void loadReceipts() {
+        // Load receipts (Fake it From Database)
+        Label receipt1 = new Label("Cuadro Bonito - 100€");
+        Label receipt2 = new Label("Cuadro Feo - 50€");
+        Label receipt3 = new Label("Cita con Vicente + 0€ Oferta especial");
+        ArrayList<Label> receipts = new ArrayList<>();
+        receipts.add(receipt1);
+        receipts.add(receipt2);
+        receipts.add(receipt3);
+        addReceiptsToPane(receipts);
+    }
+
+    private void addReceiptsToPane(ArrayList<Label> receipts) {
+        VBox vbox = new VBox();
+        vbox.setSpacing(10); // Espacio entre los recibos
+
+        for (Label receipt : receipts) {
+            if(receipt.getText().contains("+")){
+                receipt.getStyleClass().add("receipt_green"); // Aplicar la clase CSS a cada recibo
+            }
+            else{
+                receipt.getStyleClass().add("receipt_red"); // Aplicar la clase CSS a cada recibo
+            }
+            vbox.getChildren().add(receipt);
+        }
+
+        receiptsPane.getChildren().add(vbox);
     }
 
 }
