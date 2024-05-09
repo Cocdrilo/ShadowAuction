e7e60322ef4d24540263bde5491c32de1c0b69c0
Alejandro Lana Mediavilla <alejandrolana1@gmail.com>
2024-05-09 19:07:12 +0200

    Push Receipts

--- src/main/java/edu/shadowauction/shadowauction/ReceiptsController.java
+++ src/main/java/edu/shadowauction/shadowauction/ReceiptsController.java
@@ -4,8 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ReceiptsController {
 
@@ -17,6 +21,8 @@ public class ReceiptsController {
     public AnchorPane parentNode;
     @FXML
     public Label username;
+    @FXML
+    public AnchorPane receiptsPane;
 
     private FadeUtilityClass fader;
 
@@ -26,6 +32,16 @@ public class ReceiptsController {
 
     public void initialize(){
         username.setText(Usuario.getInstance(null,null).getUsername());
+
+        VBox vbox = addLabelsToVBox(createLabels());
+
+        // Hacer que el VBox se ajuste al tamaño de su contenedor padre
+        AnchorPane.setTopAnchor(vbox, 0.0);
+        AnchorPane.setRightAnchor(vbox, 0.0);
+        AnchorPane.setBottomAnchor(vbox, 0.0);
+        AnchorPane.setLeftAnchor(vbox, 0.0);
+
+        receiptsPane.getChildren().add(vbox);
     }
 
     @FXML
@@ -50,5 +66,38 @@ public class ReceiptsController {
         // Lógica para cuando el ratón sale del ImageView
         homeMessage.setOpacity(0.0);
     }
+
+    public List<Label> createLabels() {
+        List<Label> labels = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            Label label = new Label("Texto aleatorio  +" + random.nextInt(100));
+            labels.add(label);
+        }
+
+        return labels;
+    }
+
+    public VBox addLabelsToVBox(List<Label> labels) {
+        VBox vbox = new VBox();
+
+        for (Label label : labels) {
+            String style = "-fx-font-size: 20; -fx-font-weight: bold;";
+
+            if(label.getText().contains("+")){
+                style += "-fx-text-fill: green;";
+            }
+            else{
+                style += "-fx-text-fill: red;";
+            }
+
+            label.setStyle(style);
+            vbox.getChildren().add(label);
+        }
+
+        return vbox;
+    }
+
 
 }
